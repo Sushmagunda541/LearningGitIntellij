e52dfb9b81fb81f9ac10eea5e6ba46443c081d23
bandasanthosh04 <118518894+bandasanthosh04@users.noreply.github.com>
2022-11-21 11:00:32 +0530

    Update DevClass.java
    Added More changes

--- src/main/java/DevClass.java
+++ src/main/java/DevClass.java
@@ -1,6 +1,6 @@
 public class DevClass {
     public static void main(String[] args) {
-        System.out.println("Dev Class-Stream2learn");
+        System.out.println("Dev Class-Stream2learn-123");
         //checkin from stream to learn
     }
 }
